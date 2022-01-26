327560729de12707305b390afab71ddfc709ddc8
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-01-26 11:31:51 +0300

    Import components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import "./App.scss";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <div className="container">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/movie/:imdbID" component={MovieDetail} />
+          <Route component={PageNotFound} />
+        </Switch>
+        </div>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
