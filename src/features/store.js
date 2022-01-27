4746051e6079007d93942d6642bd9313bc7d079b
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-01-27 04:16:11 +0300

    Add user, update store and define movieSlice

--- src/features/store.js
+++ src/features/store.js
@@ -1,6 +1,9 @@
 import {configureStore } from "@reduxjs/toolkit"
+import moviesReducer from './movies/movieSlice'
 
 export const store = configureStore({
-    reducer:{}
+    reducer:{
+        movies:moviesReducer
+    }
 })
 
