f00a89f4e7a5c9bc79fa3cf6ce467bb4231aad79
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-01-27 05:02:22 +0300

    Use async thunk to load data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,25 +1,15 @@
 import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
-import movieApi from "../../common/apis/movieApi";
-import { APIKey } from "../../common/apis/movieApiKey";
 import { useDispatch } from "react-redux";
-import { addMovies } from "../../features/movies/movieSlice";
+import {fetchAsyncMovies, fetchAsyncShows } from "../../features/movies/movieSlice";
 
 const Home = () => {
-
   const dispatch = useDispatch()
+
   useEffect(() => {
-    const movieText = "Harry";
-    const fetchMovies = async () => {
-      const response = await movieApi
-        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
-        .catch((err) => {
-          console.log("Err :", err);
-        });
-      dispatch(addMovies(response.data))
-    };
-    fetchMovies()
-  },[]);
+    dispatch(fetchAsyncMovies())
+    dispatch(fetchAsyncShows())
+  },[dispatch]);
   return (
     <div className="banner-img">
       <MovieListing />
