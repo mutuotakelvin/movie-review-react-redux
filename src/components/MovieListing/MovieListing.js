d669d0dc5e1d325c32bfcf5e5a49794eea891c06
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-01-28 02:06:34 +0300

    Add Slice

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Slider from 'react-slick';
+import { Settings } from '../../common/setting';
 import { getAllMovies,getAllShows } from '../../features/movies/movieSlice';
 import MovieCard from '../MovieCard/MovieCard'
 import './MovieListing.scss'
 
 
 const MovieListing = () => {
+    
     const movies = useSelector(getAllMovies)
     const shows = useSelector(getAllShows)
 
@@ -31,13 +34,17 @@ const MovieListing = () => {
             <div className='movie-list'>
                 <h2>Movies</h2>
                 <div className='movie-container'>
+                    <Slider {...Settings} >
                     {renderMovies}
+                    </Slider>
                 </div>
             </div>
             <div className='show-list'>
                 <h2>Shows</h2>
                 <div className='movie-container'>
+                <Slider {...Settings} >
                     {renderShows}
+                    </Slider>
                 </div>
             </div>
         </div>
